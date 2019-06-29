2cbdec828effd61b9764795f7af9da13e98cc956
edward.yoon <eddieyooon@yahoo.com>
2019-06-29 15:03:06 -0700

    commit

---
+++ EYRZ/ClientApp/src/app/Register/RegisterComponent.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+@Component({
+    selector: 'app-register',
+    templateUrl: './register/register.component.html',
+})
+export class RegisterComponent {
+}
---
+++ EYRZ/ClientApp/src/app/Register/register.component.js
@@ -0,0 +1,8 @@
+//import { Component } from '@angular/core';
+//@Component({
+//  selector: 'register-root',
+//  templateUrl: './register/register.component.html',
+//})
+//export class RegisterComponent {
+//}
+//# sourceMappingURL=register.component.js.map
